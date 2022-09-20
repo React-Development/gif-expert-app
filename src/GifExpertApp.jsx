4dbb18c32f1badbb7650e66c1d6f161c967a8028
Brayan Garcia <bgarcia.95@outlook.com>
2022-09-20 11:38:41 -0600

    add state

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Dragon Ball",
+    "Naruto",
+  ]);
+
+  const onAddCategory = () => {
+    setCategories((prevState) => prevState.concat("Bleach"));
+  };
+
   return (
     <>
+      {/* title */}
       <h1>GifExpertApp</h1>
+
+      {/* input */}
+      <input type="text" />
+
+      {/* gif list */}
+      <button onClick={onAddCategory}>Add</button>
+      <ol>
+        {categories.map((cat) => (
+          <li key={cat}>{cat}</li>
+        ))}
+      </ol>
+      {/* gif item */}
     </>
   );
 };
