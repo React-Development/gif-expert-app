ca1a4cb211178b2650ca7be1863e7ba6b3437b12
bgarcia95 <bgarcia.95@outlook.com>
2022-09-22 16:45:08 -0600

    remove categories values from state

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,11 +4,7 @@ import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState([
-    "One Punch",
-    "Dragon Ball",
-    "Naruto",
-  ]);
+  const [categories, setCategories] = useState(["Dragon Ball"]);
 
   const onAddCategory = (category) => {
     if (categories.includes(category)) return;
