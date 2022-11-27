ec1b446bc902e39aace8586ea3b57f9014b5c7cc
bgarcia95 <bgarcia.95@outlook.com>
2022-11-26 20:29:31 -0600

    add a state, useEffect and counter example for useState

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,21 @@
+import { useState, useEffect } from "react";
+
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    getGifs(category);
+  }, [category]);
 
   return (
     <>
       <h3>{category}</h3>
+
+      <h5>{counter}</h5>
+
+      <button onClick={() => setCounter((counter) => counter + 1)}>+1</button>
     </>
   );
 };
