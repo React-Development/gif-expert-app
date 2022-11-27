33697691c5015d0e3ec3f522ba4798d3d495082a
bgarcia95 <bgarcia.95@outlook.com>
2022-11-26 20:32:50 -0600

    remove state, dependency on useEffect and html related to the counter

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,16 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  const [counter, setCounter] = useState(10);
 
   useEffect(() => {
     getGifs(category);
-  }, [category]);
+  }, []);
 
   return (
     <>
       <h3>{category}</h3>
-
-      <h5>{counter}</h5>
-
-      <button onClick={() => setCounter((counter) => counter + 1)}>+1</button>
     </>
   );
 };
