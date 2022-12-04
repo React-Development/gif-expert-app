35c8d7c5d4bf26f5cace2f8acc2e20242eb4e490
bgarcia95 <bgarcia.95@outlook.com>
2022-12-04 11:49:10 -0600

    add proptypes and fix prop

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,9 +1,16 @@
-export const GifItem = ({title, image}) => {
+import PropTypes from 'prop-types';
+
+export const GifItem = ({title, url}) => {
   
   return (
     <div className="card">
-      <img src={image} alt={title} />
+      <img src={url} alt={title} />
       <p>{title}</p>
     </div>
   )
 }
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
